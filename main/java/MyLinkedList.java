9d58f9b5617f16d73f9ae5024318ec13090e3a5d
saitarun135 <saitarun135@gm.com>
2021-03-29 09:06:49 +0530

    adding to the front

---
+++ main/java/MyLinkedList.java
@@ -0,0 +1,31 @@
+public class MyLinkedList {
+
+    public INode head;
+    public INode tail;
+
+    public MyLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void add(INode newNode) {
+        if (this.tail == null) {
+            this.tail = newNode;
+        }
+        if (this.head == null) {
+            this.head = newNode;
+        } else {
+            INode tempNode = this.head;
+            this.head = newNode;
+            this.head.setNext(tempNode);
+        }
+    }
+    public void show() {
+    	INode node=head;
+    	while(node.getNext()!=null) {
+    		System.out.println(node.getKey());
+    		node=node.getNext();
+    	}
+    	System.out.println(node.getKey());
+    }
+}
